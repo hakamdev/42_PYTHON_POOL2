62a8710696732c49d5f498dbd02c7691cac5b5fc
hakamdev <abdouhakam8@gmail.com>
2023-09-18 23:06:58 +0100

    Finish M01 ex00

---
+++ Module01/ex00/Hello.py
@@ -0,0 +1,14 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = (ft_tuple[0], "Morocco")
+ft_set = {"Hello", "Ben Guerir"}
+ft_dict["Hello"] = "1337 BG"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
