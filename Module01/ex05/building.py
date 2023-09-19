0d20b649e579bf15bb9466cb863abc577f907bcf
hakamdev <abdouhakam8@gmail.com>
2023-09-19 03:24:31 +0100

    Fix M01 ex05

--- Module01/ex05/building.py
+++ Module01/ex05/building.py
@@ -2,21 +2,43 @@ import sys
 
 
 def is_punctuation(c):
+    """Checks if a character is a punctuation mark."""
     return not (c.isalpha() or c.isdigit() or c.isspace())
 
 
-def string_analyzer(value):
-    """This function analyzes a string and prints its characters counts:
+def string_analyzer(value: str):
+    """Analyzes a string and prints its characters counts:
 - upper letters,
 - lower letters,
 - punctuation marks,
-- spaces and digits."""
+- spaces,
+- digits.
+"""
+
+    upper_count = 0
+    lower_count = 0
+    punctuation_count = 0
+    spaces_count = 0
+    digits_count = 0
+
+    for c in value:
+        if c.isupper():
+            upper_count += 1
+        elif c.islower():
+            lower_count += 1
+        elif is_punctuation(c):
+            punctuation_count += 1
+        elif c.isspace():
+            spaces_count += 1
+        elif c.isdigit():
+            digits_count += 1
+
     print(f'The text contains {len(value)} characters:')
-    print(f'{sum(1 for c in value if c.isupper())} upper letters')
-    print(f'{sum(1 for c in value if c.islower())} lower letters')
-    print(f'{sum(1 for c in value if is_punctuation(c))} punctuation marks')
-    print(f'{sum(1 for c in value if c.isspace())} spaces')
-    print(f'{sum(1 for c in value if c.isdigit())} digits')
+    print(f'{upper_count} upper letters')
+    print(f'{lower_count} lower letters')
+    print(f'{punctuation_count} punctuation marks')
+    print(f'{spaces_count} spaces')
+    print(f'{digits_count} digits')
 
 
 def main():
@@ -27,8 +49,8 @@ def main():
     elif len(sys.argv) == 2:
         value = sys.argv[1]
     else:
-        while value == '':
-            value = input("What is the text to count?\n")
+        print("What is the text to count?")
+        value = sys.stdin.readline()
     string_analyzer(value)
 
 
