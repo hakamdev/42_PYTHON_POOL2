4de433ca8fd7c71f9beae758cc1f4d71cb5f6e1d
hakamdev <abdouhakam8@gmail.com>
2023-09-19 02:12:19 +0100

    convert M01 ex04 into a script with no main

--- Module01/ex04/whatis.py
+++ Module01/ex04/whatis.py
@@ -1,17 +1,10 @@
 import sys
 
-
-def main():
-    if len(sys.argv) == 1:
-        return
-    if len(sys.argv) > 2:
-        print("AssertionError: more than one argument is provided")
-        return
+if len(sys.argv) == 2:
     try:
         num = int(sys.argv[1])
         print("I'm Even." if num % 2 == 0 else "I'm Odd.")
     except ValueError:
         print("AssertionError: argument is not an integer")
-
-
-main()
+elif len(sys.argv) > 2:
+    print("AssertionError: more than one argument is provided")
