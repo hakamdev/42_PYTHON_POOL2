648a130b15db2b3358ca8677209b9f6b8fc9d212
hakamdev <ehakam@student.1337.ma>
2023-11-30 14:07:36 +0100

    fix norm for M01 ex01

--- Module01/ex01/format_ft_time.py
+++ Module01/ex01/format_ft_time.py
@@ -4,10 +4,7 @@ from datetime import date
 time_in_ms = time.time()
 today = date.today().strftime("%b %d %Y")
 
-# Only works in Python 3.6+
 print(
-    f"Seconds since January 1, 1970: {time_in_ms:,.4f} or {time_in_ms:.2e} in scientific notation")
-# Alternative method
-# print("Seconds since January 1, 1970: {:,.4f} or {:.2e} in scientific notation".format(
-#     time_in_ms, time_in_ms))
+    "Seconds since January 1, 1970: "
+    f"{time_in_ms:,.4f} or {time_in_ms:.2e} in scientific notation")
 print(today)
