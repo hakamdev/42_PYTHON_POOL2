319286d1312a58c69cea3f9b150be1644c8bf1ec
hakamdev <ehakam@student.1337.ma>
2023-12-14 15:55:18 +0100

    Finish M03 ex01

---
+++ Module03/ex01/aff_life.py
@@ -0,0 +1,31 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def aff_life(path: str) -> None:
+    """Show a graph of life expectancy in Morocco from 1950 to 2100
+    Args:
+        path: path to the csv file
+    """
+    # Load the csv file
+    df = load(path)
+    if (df is None):
+        return None
+    # Set the country column as index
+    df.set_index("country", inplace=True)
+    # Get the Morocco data and transpose it
+    morocco_data = df.loc['Morocco'].T
+    # Plot the graph
+    morocco_data.plot(
+        title='Morocco life expectancy projections',
+        xlabel="Year",
+        ylabel='Life Expectancy')
+    plt.show()
+
+
+def main():
+    aff_life("life_expectancy_years.csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Module03/ex01/load_csv.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    if (type(path) is not str):
+        print("AssertionError: Invalid path")
+        return None
+    if (path is None or path == ""):
+        print("AssertionError: Empty path")
+        return None
+    path_segments = path.lower().split(".")
+    if (len(path_segments) < 2):
+        print("AssertionError: Invalid path")
+        return None
+    extension = path_segments[-1]
+    if (extension != "csv"):
+        print("AssertionError: Invalid path")
+        return None
+    try:
+        df = pd.read_csv(path)
+    except FileNotFoundError:
+        print(f"AssertionError: No such file or directory: '{path}'")
+        return None
+    except Exception:
+        print(f"AssertionError: failed to load: {path}")
+        return None
+    print(f"Loading dataset of dimensions {df.shape}")
+    return df
