3134af0e9e9798157576a30b88596304ae6ceb7c
hakamdev <ehakam@student.1337.ma>
2024-01-15 15:16:49 +0100

    working on M04 ex01...

---
+++ Module04/ex01/S1E7.py
@@ -0,0 +1,37 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Class to create a Baratheon character"""
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def die(self):
+        """kill a Baratheon"""
+        self.is_alive = False
+
+    # override __str__ method so it outputs: 
+    # <bound method Baratheon.__str__ of Vector: ('Baratheon', 'brown', 'dark')>
+    def __str__(self):
+        return ("Bratheon", "brown", "dark")
+
+
+class Lannister(Character):
+    """Class to create a Lannister character"""
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def die(self):
+        """kill a Lannister"""
+        self.is_alive = False
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive=True):
+        """create a Lannister"""
+        return cls(first_name, is_alive)
